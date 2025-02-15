ddcbcdf33ce5767ab2fd64df7cdfc52275225329
David <topghostly@gmail.com>
2025-02-15 21:44:50 +0100

    Add footer component and enhance global styles; implement authentication layout with responsive design

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -3,11 +3,13 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
+import styles from "@/styles/modules/auth.module.scss";
+
 const Auth: React.FC = () => {
   return (
-    <div className="login">
+    <div className={`${styles.authLayout} container `}>
       {/* LOGO NAVBAR */}
-      <div className="auth__nav">
+      <div className={styles.authLayout__nav}>
         <Navbar>
           <Link
             href={"https://lendsqr.com/"}
@@ -18,12 +20,28 @@ const Auth: React.FC = () => {
             <Image
               src={"/images/logo.svg"}
               alt="logo"
-              width={175}
+              width={174}
               height={36}
               priority
             />
           </Link>
         </Navbar>
+      </div>
+      <div className={styles.auth}>
+        <div className={styles.auth__illustration}>
+          <Image
+            src={"/images/pablo-sign-in.svg"}
+            alt="pablo sign in illustration"
+            width={600}
+            height={338}
+          />
+        </div>
+        <div className={styles.auth__form}>
+          <div className={styles.form}>
+            <h1 className={styles.auth__form_header}>Welcome</h1>
+            <p>Enter details to login.</p>
+          </div>
+        </div>
       </div>
     </div>
   );
