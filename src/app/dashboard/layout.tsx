d369274706997531c359387d15a632ef49aac9b2
David <topghostly@gmail.com>
2025-02-17 16:06:02 +0100

    Add DashboardLayout component; restructure dashboard layout with title and update styles

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,20 @@
+import Navbar from "@/components/navbar";
+import SideNav from "@/components/side-nav";
+import React from "react";
+import DashboardMain from "@/components/dashboard-main";
+import styles from "@/styles/modules/dashboard.module.scss";
+import { ChildrenLayoutProp } from "@/types/layout";
+
+const DashboardLayout: React.FC<ChildrenLayoutProp> = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      <div className={styles.dashboard}>
+        <SideNav />
+        <main className={styles.dashboard__main}>{children}</main>
+      </div>
+    </>
+  );
+};
+
+export default DashboardLayout;
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,13 @@
-import Navbar from "@/components/navbar";
-import SideNav from "@/components/side-nav";
 import React from "react";
-import DashboardMain from "@/components/dashboard-main";
-import styles from "@/styles/modules/dashboard.module.scss";
+import styles from "@/styles/components/dashboard/_dashboard-boilerplate.module.scss";
 
 const Dashboard: React.FC = () => {
   return (
     <>
-      <Navbar />
       <div className={styles.dashboard}>
-        <SideNav />
-        <DashboardMain />
+        <div className={styles.dashboard__title}>
+          <h2>Dashboard</h2>
+        </div>
       </div>
     </>
   );
